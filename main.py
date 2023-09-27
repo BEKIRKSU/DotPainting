b304b6755fb0694ee5ab38553ef0a8500cfc5921
BEKIRKSU <bekirkscu@gmail.com>
2023-09-27 16:31:50 +0100

    For loop for colors

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import colorgram
 
+rgb_colors = []
 colors = colorgram.extract('hirstpntg.jpeg', 30)
-print(colors)
+for color in colors:
+    rgb_colors.append(color.rgb)
+print(rgb_colors)
 
 
 
