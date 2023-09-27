0e7e342f339e9856b67a5739fc346e5740725137
BEKIRKSU <bekirkscu@gmail.com>
2023-09-27 16:39:27 +0100

    Colours generated

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import colorgram
 rgb_colors = []
 colors = colorgram.extract('hirstpntg.jpeg', 30)
 for color in colors:
-    rgb_colors.append(color.rgb)
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
 print(rgb_colors)
+# All of the above was done so we could generate a random list which we copied from the terminal
+# to the line below. And then we can copy out the code above because it's no longer needed.
+color_list = [(246, 242, 235), (247, 240, 244), (239, 242, 247), (237, 245, 240), (212, 149, 95), (215, 80, 62), (47, 94, 142), (231, 218, 92), (148, 66, 91), (22, 27, 40), (155, 73, 60), (122, 167, 195), (40, 22, 29), (39, 19, 15), (209, 70, 89), (192, 140, 159), (39, 131, 91), (125, 179, 141), (75, 164, 96), (229, 169, 183), (15, 31, 22), (51, 55, 102), (233, 220, 12), (159, 177, 54), (99, 44, 63), (35, 164, 196), (234, 171, 162), (105, 44, 39), (164, 209, 187), (151, 206, 220)]
+
 
 
 
